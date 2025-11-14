6243063153884ff2ee4a4faa4f9411b118ef62d8
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-11-14 19:47:42 +0100

    Formatting with clang-format

--- benchmarking/avl-benchmark.c
+++ benchmarking/avl-benchmark.c
@@ -6,16 +6,16 @@
  */
 
 #include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "avl-tree.h"
 #include "benchmark.h"
 
-int cmpShort(const void *a, const void *b) {
-  uint16_t int_a = *(uint16_t *)a;
-  uint16_t int_b = *(uint16_t *)b;
+int cmpShort(const void* a, const void* b) {
+  uint16_t int_a = *(uint16_t*)a;
+  uint16_t int_b = *(uint16_t*)b;
   if (int_a < int_b) return -1;
   if (int_a > int_b) return 1;
   return 0;
@@ -23,28 +23,22 @@ int cmpShort(const void *a, const void *b) {
 
 AVLTree tree;
 
-bool avl_add_wrapper(const void* data){
-	return avl_add(tree, data);
-}
+bool avl_add_wrapper(const void* data) { return avl_add(tree, data); }
 
-bool avl_search_wrapper(const void* data){
-	return avl_find_data(tree, data) != NULL;
-}
+bool avl_search_wrapper(const void* data) { return avl_find_data(tree, data) != NULL; }
 
-bool avl_remove_wrapper(const void* data){
-	return avl_remove(tree, data);
-}
+bool avl_remove_wrapper(const void* data) { return avl_remove(tree, data); }
 
-int main(int argc, char *argv[]) {
-	if (argc != 3 || atoi(argv[1]) <= 0 || atoi(argv[1]) >= 65536){
-		fprintf(stderr, "Usage: %s <number_of_nodes> <output_file_prefix>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
+int main(int argc, char* argv[]) {
+  if (argc != 3 || atoi(argv[1]) <= 0 || atoi(argv[1]) >= 65536) {
+    fprintf(stderr, "Usage: %s <number_of_nodes> <output_file_prefix>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-	tree = avl_new(sizeof(uint16_t), cmpShort, NULL);
+  tree = avl_new(sizeof(uint16_t), cmpShort, NULL);
 
-	benchmark(argv[2], atoi(argv[1]), &avl_add_wrapper, &avl_remove_wrapper, &avl_search_wrapper);
+  benchmark(argv[2], atoi(argv[1]), &avl_add_wrapper, &avl_remove_wrapper, &avl_search_wrapper);
 
-	avl_delete(tree);
-	return EXIT_SUCCESS;
+  avl_delete(tree);
+  return EXIT_SUCCESS;
 }
